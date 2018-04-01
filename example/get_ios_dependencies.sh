ff96ecb5476c8ff71464e6bab7340f3df4a16858
Eoin Landy <el111@yahoo.com>
2018-04-01 12:45:41 +0100

    0.0.3

--- example/get_ios_dependencies.sh
+++ example/get_ios_dependencies.sh
@@ -7,8 +7,8 @@ wget https://github.com/tuarua/Swift-IOS-ANE/releases/download/2.4.0/ios_depende
 unzip -u -o ios_dependencies.zip
 rm ios_dependencies.zip
 
-wget https://github.com/tuarua/Battery-ANE/releases/download/0.0.2/ios_dependencies.zip
+wget https://github.com/tuarua/Battery-ANE/releases/download/0.0.3/ios_dependencies.zip
 unzip -u -o ios_dependencies.zip
 rm ios_dependencies.zip
 
-wget -O ../native_extension/ane/BatteryANE.ane https://github.com/tuarua/Battery-ANE/releases/download/0.0.2/BatteryANE.ane?raw=true
+wget -O ../native_extension/ane/BatteryANE.ane https://github.com/tuarua/Battery-ANE/releases/download/0.0.3/BatteryANE.ane?raw=true
