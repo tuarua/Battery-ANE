d7a0ca426c6b731aa5563efe8d7e185fd2e96ef3
Eoin Landy <info@tuarua.co.uk>
2020-09-20 21:36:32 +0100

    remove old get scripts

--- example/get_android_dependencies.ps1
+++
@@ -1,9 +0,0 @@
-$AneVersion = "1.5.0"
-$FreKotlinVersion = "1.40.0"
-$GsonVersion = "2.8.6"
-
-$currentDir = (Get-Item -Path ".\" -Verbose).FullName
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri https://github.com/tuarua/Battery-ANE/releases/download/$AneVersion/BatteryANE.ane?raw=true -OutFile "$currentDir\..\native_extension\ane\BatteryANE.ane"
-Invoke-WebRequest -OutFile "$currentDir\android_dependencies\com.google.code.gson.gson-$GsonVersion.ane" -Uri https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/misc/com.google.code.gson.gson-$GsonVersion.ane?raw=true
-Invoke-WebRequest -OutFile "$currentDir\android_dependencies\com.tuarua.frekotlin-$FreKotlinVersion.ane" -Uri https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/kotlin/com.tuarua.frekotlin-$FreKotlinVersion.ane?raw=true
--- example/get_android_dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-AneVersion="1.5.0"
-FreKotlinVersion="1.40.0"
-GsonVersion="2.8.6"
-
-wget -O android_dependencies/com.tuarua.frekotlin-$FreKotlinVersion.ane https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/kotlin/com.tuarua.frekotlin-$FreKotlinVersion.ane?raw=true
-wget -O android_dependencies/com.google.code.gson.gson-$GsonVersion.ane https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/misc/com.google.code.gson.gson-$GsonVersion.ane?raw=true
-wget -O ../native_extension/ane/BatteryANE.ane https://github.com/tuarua/Battery-ANE/releases/download/${AneVersion}/BatteryANE.ane?raw=true
